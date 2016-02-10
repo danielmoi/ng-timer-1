c0592ad9d907f55f94f82869061d0257dc703a5a
danielmoi <daniel@ampersandmoi.com>
2016-02-10 15:25:34 +1100

    Unsuccessfully make remaining_ms a computed value

--- src/js/timer-2.js
+++ src/js/timer-2.js
@@ -32,39 +32,34 @@ app.controller('myController', ['$scope', function ($scope) {
     running = 0,
     fresh = 1;
 
-  $scope.input_pomo_min = 0.2;
+  $scope.input_pomo_min = 1;
   $scope.input_break_min = 0.3;
 
-//  $scope.initial_ms_pomo = $scope.input_pomo_min * 1000 * 60;
-//  $scope.initial_ms_break = $scope.input_break_min * 1000 * 60;
-  
-  $scope.$watch('input_pomo_min', function(value) {
+  //  $scope.initial_ms_pomo = $scope.input_pomo_min * 1000 * 60;
+  //  $scope.initial_ms_break = $scope.input_break_min * 1000 * 60;
+
+  $scope.$watch('input_pomo_min', function (value) {
     $scope.initial_ms_pomo = value * 1000 * 60;
   });
-  
-  $scope.$watch('input_break_min', function(value) {
+
+  $scope.$watch('input_break_min', function (value) {
     $scope.initial_ms_break = value * 1000 * 60;
   });
 
-  $scope.remaining_ms_pomo = '';
-  $scope.remaining_ms_break = '';
-
-  $scope.initial_ms = '';
+  $scope.initial_ms = $scope.input_pomo_min * 1000 * 60;
   $scope.current_ms = '';
-  $scope.remaining_ms = '';
+  $scope.remaining_ms = $scope.input_pomo_min * 1000 * 60;
 
-  $scope.displayable_time = '';
-  
-  $scope.myDisplay = function(ms) {
-    ms = $scope.remaining_ms;
-    display_min = Math.floor(ms / 60000);
-    display_sec = Math.floor(ms / 1000) % 60;
+  $scope.$watch('remaining_ms', function (value) {
+
+    display_min = Math.floor(value / 60000);
+    display_sec = Math.floor(value / 1000) % 60;
     display_min = display_min < 10 ? '0' + display_min : display_min;
     display_sec = display_sec < 10 ? '0' + display_sec : display_sec;
-    return display_min + ':' + display_sec;
-  };
+    $scope.displayable_time = display_min + ':' + display_sec;
+    console.log($scope.displayable_time);
 
-
+  });
   $scope.start = function () {
     if (running === 0) {
       referenceTime = Date.now();
@@ -73,13 +68,13 @@ app.controller('myController', ['$scope', function ($scope) {
         fresh = 0;
         if (session === 'pomo') {
           console.log('starting new POMO!');
-          $scope.current_ms = $scope.initial_ms_pomo;
           $scope.initial_ms = $scope.initial_ms_pomo;
+          $scope.current_ms = $scope.initial_ms_pomo;
         }
         if (session === 'break') {
           console.log('starting new BREAK!');
-          $scope.current_ms = $scope.initial_ms_break;
           $scope.initial_ms = $scope.initial_ms_break;
+          $scope.current_ms = $scope.initial_ms_break;
         }
       }
 
@@ -88,28 +83,23 @@ app.controller('myController', ['$scope', function ($scope) {
           actualInterval = checkTime - referenceTime;
         $scope.remaining_ms = $scope.current_ms - actualInterval;
         console.log(session + ': ' + $scope.remaining_ms);
-        
-        $scope.$apply(function () {
-          $scope.convert_ms($scope.remaining_ms);
-        });
+        console.log($scope.displayable_time);
+        $scope.$digest();
+        //        $scope.displayable_time_2();
+
+        //        $scope.$apply(function () {
+        //          $scope.convert_ms($scope.current_ms);
+        //        });
 
         if ($scope.remaining_ms <= 0) {
           $scope.stop();
           $scope.finished();
         }
-      }, 500);
+      }, 1000);
     }
   };
-  
-  $scope.convert_ms = function (ms) {
-    display_min = Math.floor(ms / 60000);
-    display_sec = Math.floor(ms / 1000) % 60;
-    display_min = display_min < 10 ? '0' + display_min : display_min;
-    display_sec = display_sec < 10 ? '0' + display_sec : display_sec;
-    $scope.displayable_time = display_min + ':' + display_sec;
 
-    console.log('displayable_time: ' + $scope.displayable_time);
-  };
+
   $scope.stop = function () {
     if (running === 1) {
       running = 0;
@@ -145,8 +135,10 @@ app.controller('myController', ['$scope', function ($scope) {
     fresh = 1;
     clearInterval(intervalID);
     console.log($scope.initial_ms);
+    $scope.current_ms = $scope.initial_ms;
+    $scope.remaining_ms = $scope.initial_ms;
 
-    
+
     console.log('reset, intervalID: ' + intervalID);
 
 
